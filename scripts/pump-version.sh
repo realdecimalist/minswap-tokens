e3732a51b47043f2fa09f2fbe4a6982a880d298f
James Ng <theuranus.ntb@gmail.com>
2024-11-27 15:32:36 +0700

    add script pump-version
    Signed-off-by: James Ng <theuranus.ntb@gmail.com>

---
+++ scripts/pump-version.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=$1
+
+echo "update version field"
+jq ".version = \"$version\"" <package.json >package.json.tmp
+mv package.json.tmp package.json
+
+echo "build"
+npm run build
+
+echo "publish"
+npm publish
+
+echo "commit new version"
+git add package.json
+git commit -m "publish version v$version"
+
+echo "tag version"
+git tag v"$version"
+
+echo "push to remote"
+git push origin main
+git push origin v"$version"
